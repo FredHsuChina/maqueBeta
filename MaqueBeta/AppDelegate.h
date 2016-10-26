4525ac2e2e0dca3ec94dd5717fe6f747e154cd3e
FredHsu <FredHsu0822@hotmail.com>
2016-10-26 17:26:11 +0800

    Initial Commit

---
+++ MaqueBeta/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MaqueBeta
+//
+//  Created by 许文锋 on 16/10/26.
+//  Copyright © 2016年 fred. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MaqueBeta/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MaqueBeta
+//
+//  Created by 许文锋 on 16/10/26.
+//  Copyright © 2016年 fred. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
