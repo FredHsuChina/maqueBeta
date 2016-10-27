854090f68e42aa3b0a054d98d09ee1465cc9bfb1
FredHsu <FredHsu0822@hotmail.com>
2016-10-27 16:50:16 +0800

    1

--- MaqueBeta/AppDelegate.m
+++ MaqueBeta/AppDelegate.m
@@ -7,6 +7,7 @@
 //
 
 #import "AppDelegate.h"
+#import "ViewController.h"
 
 @interface AppDelegate ()
 
@@ -17,6 +18,11 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    [[UIApplication sharedApplication] setStatusBarStyle:UIStatusBarStyleLightContent];
+    ViewController *VC = [[ViewController alloc] init];
+    UINavigationController *nav = [[UINavigationController alloc] initWithRootViewController:VC];
+    self.window.rootViewController = nav;
+    
     return YES;
 }
 
---
+++ MaqueBeta/MainViewController.h
@@ -0,0 +1,16 @@
+//
+//  MainViewController.h
+//  MaqueBeta
+//
+//  Created by 许文锋 on 16/10/27.
+//  Copyright © 2016年 fred. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MainViewController : UIViewController
+
+@property(nonatomic,strong) NSString *urlName;
+
+@property(nonatomic,assign) BOOL noNeedPermit;
+@end
